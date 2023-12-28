1c24aabc6b7b130171d59174f4c812eb7e01b52e
Twinispro <matthewmaliqu@gmail.com>
2023-12-28 00:10:59 -0500

    Testing Out Things
    Just messing around with stuff

---
+++ product.js
@@ -0,0 +1,20 @@
+const products = [
+        {name: "Bread", price: '$7'},
+        {name: "Cheese", price: '$4'},
+        {name: "Ham", price: '$12'},
+        {name: "Lettuce", price: '$2'}
+]
+
+
+//functions to add a new product
+
+function addProduct(name, price){
+    const newProduct = {name, price};
+    products.push(newProduct)
+}
+
+//Function to get all products
+
+function getAllProducts(){
+    return products;
+}
---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded',  function(){
+    const search = document.getElementById('search');
+    const searchResults = document.getElementById('searchResults')
+    const cardTemplate = document.getElementById('card-template').innerHTML;
+
+    //Event listener when text is place in the search bar
+    search.addEventListener('input', function(){
+    const searchTerm = search.value.toLowerCase();
+    const filteredResults = getFilteredResults(searchTerm);
+    displayResults(filteredResults);
+    });
+
+    function getFilteredResults(term){
+        //test data
+        const allResults = getAllProducts();
+        return allResults.filter(result => result.name.toLowerCase().includes(term));
+    }
+    
+    function displayResults(results){
+        searchResults.innerHTML = '';
+
+        results.forEach(result => {
+         const cardHTML = cardTemplate
+         .replace('{{name}}', result.name)
+         .replace('{{price}}', result.price);
+
+         const card = document.createRange().createContextualFragment(cardHTML).querySelector('.card')
+
+            searchResults.appendChild(card);
+
+            card.addEventListener('click', function(){
+                alert(`You clicked on ${result.name}`)
+            });
+        });
+
+    }
+});
